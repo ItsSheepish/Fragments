b6174b56e1dadb42cc49952a2eacbb5a75591251
LarryPlayz <romanblundell@gmail.com>
2024-05-16 15:51:47 +0100

    Easier Shear Upgrade

--- src/main/java/its/sheepish/fragments/util/ModLootTableModifiers.java
+++ src/main/java/its/sheepish/fragments/util/ModLootTableModifiers.java
@@ -12,13 +12,59 @@ import net.minecraft.util.Identifier;
 
 public class ModLootTableModifiers {
     private static Identifier SHEPHERD_ID = new Identifier("minecraft","chests/village/village_shepherd");
+    private static Identifier PLAINS = new Identifier("minecraft","chests/village/village_plains_house");
+    private static Identifier DESERT = new Identifier("minecraft","chests/village/village_desert_house");
+    private static Identifier SNOWY = new Identifier("minecraft","chests/village/village_snowy_house");
+    private static Identifier TAIGA = new Identifier("minecraft","chests/village/village_taiga_house");
+    private static Identifier SAVANNA = new Identifier("minecraft","chests/village/village_savanna_house");
+
 
     public static void modifyLootTables() {
         LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
             if(SHEPHERD_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(0.35f)) // Drops 100% of the time
+                        .conditionally(RandomChanceLootCondition.builder(0.35f))
+                        .with(ItemEntry.builder(ModItems.DiamondUpgrade))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+
+                tableBuilder.pool(poolBuilder.build());
+            } else if(PLAINS.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.075f))
+                        .with(ItemEntry.builder(ModItems.DiamondUpgrade))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+
+                tableBuilder.pool(poolBuilder.build());
+            } else if(DESERT.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.075f))
+                        .with(ItemEntry.builder(ModItems.DiamondUpgrade))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+
+                tableBuilder.pool(poolBuilder.build());
+            } else if(SNOWY.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.075f))
+                        .with(ItemEntry.builder(ModItems.DiamondUpgrade))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+
+                tableBuilder.pool(poolBuilder.build());
+            } else if(TAIGA.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.075f))
+                        .with(ItemEntry.builder(ModItems.DiamondUpgrade))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+
+                tableBuilder.pool(poolBuilder.build());
+            } else if(SAVANNA.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.075f))
                         .with(ItemEntry.builder(ModItems.DiamondUpgrade))
                         .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
 
